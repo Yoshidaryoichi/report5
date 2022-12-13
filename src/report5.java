cc3c43fc0321c0621f7c8e4e21b4d7f2e5074cc4
Yoshida Ryoichi <e225735@ie.u-ryukyu.ac.jp>
2022-12-13 20:42:35 +0900

    second commit

--- src/report5.java
+++ src/report5.java
@@ -1,8 +1,11 @@
 public class report5{
-    public static void main(String[] args) {
-    String str = null;
-    
-    System.out.println(str.length());
+    public static void main(String[] args){
+    try {
+        String str = null;
+        System.out.println(str.length());
+    }  catch (NullPointerException e) {
+        System.out.println("NullPointerExceptionが発生した"+e.getMessage());
+
     }
-    
+}
 }
